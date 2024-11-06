c060fb67e16617242a0e913b70f431036b3e724e
thedevethan <tellabokame@gmail.com>
2024-11-06 19:06:40 +0000

    Search party ok

--- desktop-template/script.js
+++ desktop-template/script.js
@@ -19,10 +19,40 @@ tippy(".profil_pic", {
 
 // Search box
 let search = document.querySelector('#search'),
-    closeBtn = document.querySelector('.close');
+    closeBtn = document.querySelector('.close'),
+    searchBox = document.querySelector('.search-box'),
+    loupe = document.querySelector('.loupe');
+    // option = ["html","css","javascript","php","react","vue","angular","node.js","express","mongodb","sql","python","java","swift","kotlin","android","ios","flutter","git","github","docker","jenkins","aws","azure","linux","bash","vscode","sublime text","webpack","npm","yarn","babel","firebase","graphql","rest api","ui/ux","design patterns","debugging","testing","performance optimization","ci/cd","machine learning","ai tools","cloud storage","version control",];
+
+// function Search(options, entry) {
+//     let goodOptions = [];
+//     options.forEach((item) => {
+//         if (item.includes(entry)) {
+//             goodOptions.push(item)
+//         }
+//     });
+//     return goodOptions;
+// }
+
+// searchBox.addEventListener("click", () => {
+//     searchBox.style.transition = "0.2s";
+//     searchBox.style.border = "1px solid white";
+// })
 
 search.addEventListener("input", () => {
-    closeBtn.style.visibility = "visible"
+    closeBtn.style.visibility = "visible";
+})
+
+search.addEventListener("focus", () => {
+    searchBox.style.transition = "0.2s";
+    searchBox.style.border = "1px solid white";
+    loupe.setAttribute("src", "../image/magnifier.png")
+})
+
+search.addEventListener("blur", () => {
+    searchBox.style.transition = "0.2s";
+    searchBox.style.border = "1px solid #9198a1a6";
+    loupe.setAttribute("src", "../image/loupe.png")
 })
 
 closeBtn.addEventListener("click", () => {

53f18d855bfc64aa67dfeb177f5c92a043a35233
ethanBokame <ethaniraqui@gmail.com>
2024-11-28 14:10:45 +0100

    premier essai 'auto deployment

--- index.php
+++ index.php
@@ -1,27 +1,5 @@
 <?php 
-require "vendor/autoload.php";
-$router = new AltoRouter();
 
-// map homepage
-$router->map('GET', '/debugger_life/', function() {
-    require 'desktop-template-php/explorer.php';
-});
-
-
-$match = $router->match();
-
-// var_dump($match["target"]);
-
-// call closure or throw 404 status
-if( is_array($match) && is_callable( $match['target'] ) ) {
-	call_user_func_array( $match['target'], $match['params'] );
-} else {
-	// no route was matched
-	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
-}
-
-
-
-// var_dump($_SERVER['REQUEST_URI']);
+echo "ça marche !"
 
 ?>

87fc3ac15735b48695307d6ccdfad424e906b517
ethanBokame <ethaniraqui@gmail.com>
2024-11-29 09:21:35 +0100

    changement dynamique de base de données en fonction de l'environnement d'exécution

--- desktop-template-php/conn.php
+++ desktop-template-php/conn.php
@@ -1,10 +1,20 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+if ($_SERVER["HTTP_HOST"] == "localhost") {
+    $servername = "localhost";
+    $dbname = "debuggerlife";
+    $username = "root";
+    $password = "";
+}
+else {
+    $servername = "localhost";
+    $dbname = "u952366364_debuggerlife";
+    $username = "u952366364_admindbl";
+    $password = "Footballdeethvn10$";
+}
+
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=debuggerlife", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
